e5625c179da89f6592328056be87988b2cd3cfb9
Carl Mark Carambas <36352083+carlcarambas@users.noreply.github.com>
2025-05-06 15:51:45 +0800

    add image interceptor

--- apps/api/src/app/pigeons/pigeons.controller.ts
+++ apps/api/src/app/pigeons/pigeons.controller.ts
@@ -6,11 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { PigeonsService } from './pigeons.service';
 import { Pigeon as PigeonModel, Prisma } from '@prisma/client';
 import { CreatePigeonDto } from './pigeons.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @ApiTags('pigeons')
 @Controller('pigeons')
@@ -18,6 +22,32 @@ export class PigeonsController {
   constructor(private readonly pigeonsService: PigeonsService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Pigeon created successfully',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  async createv1(@Body() data: CreatePigeonDto) {
+    // TODO should get the ownerId from the request
+    const ownerId = '6805f27f9d314aa072dad66a';
+    return this.pigeonsService.create({ ...data, ownerId });
+  }
+
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('images', 5, {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          return callback(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+    })
+  )
   @ApiResponse({
     status: 201,
     description: 'Pigeon created successfully',
